8b0856b56b6a6e52d73ed60894d5751cf31b3cfd
[Jayabrata Das] <[dhyanabratad22@gmail.com]>
2024-11-28 23:19:37 +0530

    Add project files.

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,75 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly EcommerceContext ecommerceContext;
+
+        public ProductController(EcommerceContext ecommerceContext)
+        {
+            this.ecommerceContext = ecommerceContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+        {
+           return await ecommerceContext.Products.ToListAsync();
+            
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> getproductByid(int id)
+        {
+            var product = ecommerceContext.Products.FindAsync(id);
+            if(product != null)
+            {
+                return await product;
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Createproduct(Product product)
+        {
+            ecommerceContext.Products.AddAsync(product);
+
+            await ecommerceContext.SaveChangesAsync();  
+            return product;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> updateProduct(int id, Product product)
+        {
+            if(product.Id != id || !ProductExists(id))
+                return BadRequest("Can not Update Product");
+
+            ecommerceContext.Entry(product).State = EntityState.Modified;
+            await ecommerceContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        private bool ProductExists(int id)
+        {
+           return ecommerceContext.Products.Any(p => p.Id == id);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var findproduct = await ecommerceContext.Products.FindAsync(id);
+            if(findproduct == null) return NotFound();
+
+            ecommerceContext.Remove(findproduct);
+            await ecommerceContext.SaveChangesAsync();
+            return Ok();
+
+        }
+    }
+}
---
+++ Core/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        //public int ProductId { get; set; }
+        public required string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public required string Description { get; set; }
+        public required string Type {  get; set; }
+        public required string PictureUrl { get; set; }
+        public required string Brand {  get; set; }
+    }
+}
